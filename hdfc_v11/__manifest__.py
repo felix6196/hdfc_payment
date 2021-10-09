bf522138c8417caa240a191c41ffa51a4110f0c2
felix6196 <52097047+felix6196@users.noreply.github.com>
2021-10-09 12:28:28 +0530

    Add files via upload
    added the v11 code

---
+++ hdfc_v11/__manifest__.py
@@ -0,0 +1,32 @@
+{
+    'name' : 'HDFC Payment Gateway',
+    'version' : '11.0.1.0.0',
+    'summary': 'It helps to Integrate your ecommerce with HDFC Payment Gateway',
+    'sequence': -1,
+    'description': """
+    """,
+    'category': 'Payment Gateway',
+    'author': 'agaramsoft',
+    'website': 'https://agaramsoft.com/',
+    'images' : 'static/description/icon.png',
+    'live_test_url':'',
+    'license': 'AGPL-3',
+    'price': 1.00,
+    'currency': 'USD',
+    'depends' : ['base','website_sale', 'payment'],
+    'data': [
+        'views/payment_acquirer_view.xml',
+        'views/template.xml',
+        'views/hdfc_payment_template.xml',
+        # 'views/assets.xml',
+        'data/hdfc.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [
+    ],
+    'images': ['static/description/icon.png'],  
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
